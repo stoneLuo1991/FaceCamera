3798fecfd2a9528ee398be5bd1ff2f4a4e8b8f0e
stoneLuo1991 <931805308@qq.com>
2016-11-17 16:42:06 +0800

    First commit!

---
+++ app/src/main/java/android/luo/stone/facealigncamera/MainActivity.java
@@ -0,0 +1,13 @@
+package android.luo.stone.facealigncamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
